1eb5af3b0f41e18eaa011ac2814b94c30320c6df
Hema <hemalathadevarajan5@gmail.com>
2026-07-06 23:40:21 +0530

    Create Strong number.c

---
+++ Number Series/Strong number.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,temp,r,fact,sum=0,i;
+
+    scanf("%d",&n);
+
+    temp=n;
+
+    while(temp>0)
+    {
+        r=temp%10;
+        fact=1;
+
+        for(i=1;i<=r;i++)
+        {
+            fact=fact*i;
+        }
+
+        sum=sum+fact;
+        temp=temp/10;
+    }
+
+    if(sum==n)
+        printf("Strong Number");
+    else
+        printf("Not Strong Number");
+
+    return 0;
+}

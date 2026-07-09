a9f10ddab1f63b247abaad1ab2e56d5b645fbc22
Hema <hemalathadevarajan5@gmail.com>
2026-07-09 20:50:11 +0530

    Create Odd Fibonacci series.c

---
+++ Number Series/Odd Fibonacci series.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n, a = 0, b = 1, c, i;
+    scanf("%d", &n);
+    if(a % 2 != 0)
+        printf("%d ", a);
+    if(b % 2 != 0)
+        printf("%d ", b);
+    for(i = 3; i <= n; i++) {
+        c = a + b;
+        if(c % 2 != 0)
+            printf("%d ", c);
+        a = b;
+        b = c;
+    }
+    return 0;
+}

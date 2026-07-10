6511d5e8814f39cbc0b251c38d30b71fe63111ea
Hema <hemalathadevarajan5@gmail.com>
2026-07-10 22:15:14 +0530

    Create String compare.c

---
+++ Strings/String compare.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    char str1[100], str2[100];
+    int i = 0;
+    scanf("%s", str1);
+    scanf("%s", str2);
+    while(str1[i] != '\0' && str2[i] != '\0')
+    {
+        if(str1[i] != str2[i])
+        {
+            printf("Not Equal");
+            return 0;
+        }
+        i++;
+    }
+    if(str1[i] == '\0' && str2[i] == '\0')
+        printf("Equal");
+    else
+        printf("Not Equal");
+    return 0;
+}

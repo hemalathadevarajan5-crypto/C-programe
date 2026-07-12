a55b4dc356e4a8d0213c0f078e0e3d95e878ce77
Hema <hemalathadevarajan5@gmail.com>
2026-07-12 09:14:19 +0530

    Create Find the position of the char.c

---
+++ Strings/Find the position of the char.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    char str[100], ch;
+    int i = 0;
+    scanf("%s", str);
+    scanf(" %c", &ch);
+    while(str[i] != '\0')
+    {
+        if(str[i] == ch)
+        {
+            printf("%d", i);
+            return 0;
+        }
+        i++;
+    }
+    printf("Not found");
+    return 0;
+}

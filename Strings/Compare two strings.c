bd18aea8de60f186907c3b9d77214a4b89d1d7f8
Hema <hemalathadevarajan5@gmail.com>
2026-07-13 22:31:41 +0530

    Create Compare two strings.c

---
+++ Strings/Compare two strings.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char str1[100],str2[100];
+    int i=0;
+    scanf("%s%s",str1,str2);
+    while(str1[i]==str2[i] && str1[i]!='\0')
+    {
+        i++;
+    }
+    if(str1[i]==str2[i])
+        printf("Equal");
+    else
+        printf("Not Equal");
+    return 0;
+}

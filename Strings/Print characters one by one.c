af481f2aa83bcdb0dfadf0300aff7b1e249d1f9a
Hema <hemalathadevarajan5@gmail.com>
2026-07-14 23:27:41 +0530

    Create Print characters one by one.c

---
+++ Strings/Print characters one by one.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i=0;
+    scanf("%s",str);
+    while(str[i]!='\0')
+    {
+        printf("%c\n",str[i]);
+        i++;
+    }
+    return 0;
+}

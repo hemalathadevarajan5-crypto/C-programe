cea0134f8845da4678ba5bddd31901e794d71a6a
Hema <hemalathadevarajan5@gmail.com>
2026-07-15 21:37:20 +0530

    Create Find middle character.c

---
+++ Strings/Find middle character.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i=0;
+    scanf("%s",str);
+    while(str[i]!='\0')
+    {
+        i++;
+    }
+    printf("%c",str[i/2]);
+    return 0;
+}

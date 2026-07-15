9a928a1a558009590535290f7218ae685f721ecb
Hema <hemalathadevarajan5@gmail.com>
2026-07-15 21:38:14 +0530

    Create Delete character at given position.c

---
+++ Strings/Delete character at given position.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i,pos;
+    scanf("%s",str);
+    scanf("%d",&pos);
+    for(i=pos;str[i]!='\0';i++)
+    {
+        str[i]=str[i+1];
+    }
+    printf("%s",str);
+    return 0;
+}

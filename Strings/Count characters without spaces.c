b9409e025ae731d590ef2cc44978be5f874d7ea1
Hema <hemalathadevarajan5@gmail.com>
2026-07-16 23:01:06 +0530

    Create Count characters without spaces.c

---
+++ Strings/Count characters without spaces.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    char str[100];
+    int i=0,count=0;
+
+    scanf("%[^\n]",str);
+
+    while(str[i]!='\0')
+    {
+        if(str[i]!=' ')
+            count++;
+
+        i++;
+    }
+
+    printf("%d",count);
+
+    return 0;
+}

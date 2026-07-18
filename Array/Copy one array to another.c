c87b54176e9dc2ad10447ec8174db581c703cdc5
Hema <hemalathadevarajan5@gmail.com>
2026-07-18 19:38:47 +0530

    Create Copy one array to another.c

---
+++ Array/Copy one array to another.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[100],b[100],n,i;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0;i<n;i++)
+    {
+        b[i]=a[i];
+    }
+
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",b[i]);
+    }
+
+    return 0;
+}

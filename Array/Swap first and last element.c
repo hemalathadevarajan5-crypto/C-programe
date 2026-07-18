41ce583da9c67766110bb46f183012d865677956
Hema <hemalathadevarajan5@gmail.com>
2026-07-18 19:45:10 +0530

    Create Swap first and last element.c

---
+++ Array/Swap first and last element.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[100],n,i,temp;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    temp=a[0];
+    a[0]=a[n-1];
+    a[n-1]=temp;
+
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+
+    return 0;
+}

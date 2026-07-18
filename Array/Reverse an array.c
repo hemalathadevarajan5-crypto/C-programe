221a247884a800c19770bd956c2a361601eb95e9
Hema <hemalathadevarajan5@gmail.com>
2026-07-18 19:38:12 +0530

    Create Reverse an array.c

---
+++ Array/Reverse an array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[100],n,i,temp;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0;i<n/2;i++)
+    {
+        temp=a[i];
+        a[i]=a[n-i-1];
+        a[n-i-1]=temp;
+    }
+
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+
+    return 0;
+}

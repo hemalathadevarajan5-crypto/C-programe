8f7f922ca200e5ac076f4cbaf516cd0a8634f855
Hema <hemalathadevarajan5@gmail.com>
2026-07-18 19:47:39 +0530

    Create Print position of an element.c

---
+++ Array/Print position of an element.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[100],n,i,key;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    scanf("%d",&key);
+
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==key)
+        {
+            printf("%d",i+1);
+            return 0;
+        }
+    }
+
+    printf("-1");
+
+    return 0;
+}

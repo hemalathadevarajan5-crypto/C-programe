4b077f6b94ef7ac4a71fc7568afc1bb74e5ea414
Hema <hemalathadevarajan5@gmail.com>
2026-07-18 22:46:59 +0530

    Create Binary search.c

---
+++ Array/Binary search.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[100],n,i,key,low,high,mid;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    scanf("%d",&key);
+
+    low=0;
+    high=n-1;
+
+    while(low<=high)
+    {
+        mid=(low+high)/2;
+
+        if(a[mid]==key)
+        {
+            printf("%d",mid);
+            return 0;
+        }
+        else if(a[mid]<key)
+        {
+            low=mid+1;
+        }
+        else
+        {
+            high=mid-1;
+        }
+    }
+
+    printf("-1");
+
+    return 0;
+}

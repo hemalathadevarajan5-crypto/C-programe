b98c68795a6419185b18da13eafeabb7f79c0e19
Hema <hemalathadevarajan5@gmail.com>
2026-07-19 19:44:24 +0530

    Create Count multiples of 10.c

---
+++ Array/Count multiples of 10.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[100],n,i,count=0;
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+
+        if(a[i]%10==0)
+        {
+            count++;
+        }
+    }
+
+    printf("%d",count);
+
+    return 0;
+}

de9dfad4c062c6b0957b887a05302a1696ecce7e
Hema <hemalathadevarajan5@gmail.com>
2026-07-24 21:24:40 +0530

    Create Check Symmetric matrix using transpose.c

---
+++ Matrix/Check Symmetric matrix using transpose.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[10][10],n,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(a[i][j]!=a[j][i])
+            {
+                printf("Not Symmetric");
+                return 0;
+            }
+        }
+    }
+    printf("Symmetric");
+    return 0;
+}

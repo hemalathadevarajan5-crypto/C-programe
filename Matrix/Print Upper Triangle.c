68476ce6a0bcbd71852fe14bf7e808036674171e
Hema <hemalathadevarajan5@gmail.com>
2026-07-24 21:22:41 +0530

    Create Print Upper Triangle.c

---
+++ Matrix/Print Upper Triangle.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[10][10],n,i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i;j<n;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
